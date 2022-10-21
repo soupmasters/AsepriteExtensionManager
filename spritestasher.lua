449d182a4c99ca54220c59e2e9bbed5de48a755b
Martin Calander <martin@soupmasters.com>
2022-10-22 00:55:42 +0200

    Script Stasher Finished

--- spritestasher.lua
+++ spritestasher.lua
@@ -1,8 +1,19 @@
-function file_exists(name)
-    local f=io.open(name,"r")
-    if f~=nil then io.close(f) return true else return false end
+local spriteStashLocation = "/Users/martin/Dropbox/[ Big Boy Boxing ]/Artwork/Characters/TestScriptStasherLALALA/" -- "/Users/martin/Dropbox/[ Big Boy Boxing ]/Artwork/Characters/TestScriptStasherLALALA/"
+local extension = ".ase" --.ase or .aseprite
+
+function sendToSpriteStash()
+    newLocation = spriteStashLocation .. app.fs.fileTitle(app.activeSprite.filename)
+    if not app.fs.isFile(newLocation .. extension) then
+        app.activeSprite:saveCopyAs(newLocation .. extension)
+    else
+        for i = 1,1000,1
+        do
+            if not app.fs.isFile(newLocation .. " (" .. tostring(i) .. ")" .. extension) then
+                app.activeSprite:saveCopyAs(newLocation .. " (" .. tostring(i) .. ")" .. extension)
+                return
+            end
+        end
+    end
 end
-local pathPart = app.fs.filePath(fn)
-local oldFile = "/Users/martin/Desktop"
-local newFile = ""
---os.execute(string.format('copy "%s" "%s"', oldFile, newFile))
+
+sendToSpriteStash()

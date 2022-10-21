259640d097ed01c11f21dca629a25b9c5558031a
Martin Calander <martin@soupmasters.com>
2022-10-21 13:50:56 +0200

    Update bitfrosttester.lua

--- bitfrosttester.lua
+++ bitfrosttester.lua
@@ -3,19 +3,27 @@ local dlg = Dialog()
 
 local function receive(t, message)
     if t == WebSocketMessageType.OPEN then
+        ws:sendText("Aseprite Connect")
         dlg:modify{id="status", text="Sync ON"}
-        print("in soup i like")
-        ws:sendText("as+´´oa+ifh+fwhi!")
+
+    elseif t == WebSocketMessageType.TEXT then
+        dlg:modify{id="status", text="Aseprite Text: " .. message}
 
     elseif t == WebSocketMessageType.CLOSE then
-        dlg:modify{id="status", text="No connection"}
+        ws:sendText("Aseprite Disconnect")
+        dlg:modify{id="status", text="Connection closed"}
     end
 end
 
+-- clean up and exit
+local function finish()
+    ws:close()
+    dlg:close()
+end
 
 -- set up a websocket
-ws = WebSocket{ url="ws://127.0.0.1:8812/Laputa", onreceive=receive, deflate=false }
-
+ws = WebSocket{ url="ws://127.0.0.1:8814/Laputa", onreceive=receive, deflate=false }
+--ws://127.0.0.1:8812/Laputa
 -- create an UI
 dlg:label{ id="status", text="Connecting..." }
 dlg:button{ text="Cancel", onclick=finish}

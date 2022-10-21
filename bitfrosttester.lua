0cf32890d8a9acbf517ed628917697bc6d35b3f5
Martin Calander <martin@soupmasters.com>
2022-10-21 19:42:41 +0200

    Test

--- bitfrosttester.lua
+++ bitfrosttester.lua
@@ -1,37 +1,5 @@
-local ws -- websocket created later
-local dlg = Dialog()
-
-local function receive(t, message)
-    if t == WebSocketMessageType.OPEN then
-        ws:sendText("Aseprite Connect")
-        dlg:modify{id="status", text="Sync ON"}
-
-    elseif t == WebSocketMessageType.TEXT then
-        dlg:modify{id="status", text="Aseprite Text: " .. message}
-        
-    elseif t == WebSocketMessageType.CLOSE then
-        ws:sendText("Aseprite Disconnect")
-        dlg:modify{id="status", text="Connection closed"}
-    end
-end
-
-local function send()
-    ws:sendText("Iam seing some data beepboop")
-end
--- clean up and exit
-local function finish()
-    ws:close()
-    dlg:close()
-end
-
--- set up a websocket
-ws = WebSocket{ url="ws://127.0.0.1:8814/Laputa", onreceive=receive, deflate=false }
---ws://127.0.0.1:8812/Laputa
--- create an UI
-dlg:label{ id="status", text="Connecting..." }
-dlg:button{ text="Cancel", onclick=finish}
-dlg:button{ text="SendData", onclick=send}
-
--- let's go!
-ws:connect()
-dlg:show{ wait=false }
+app.range:clear()
+app.range.frames = { 4 ... 14 }
+app.activeFrame = 4
+--os.execute("sleep " .. tonumber(0.04))
+app.command.GotoFirstFrameInTag()
--- dep.lua
+++ dep.lua
@@ -50,3 +50,101 @@ dlg:button {
 }
 
 dlg:show { wait = false }
+
+------
+
+local ws -- websocket created later
+local dlg = Dialog()
+
+local function receive(t, message)
+    if t == WebSocketMessageType.OPEN then
+        ws:sendText("Aseprite Connect")
+        dlg:modify{id="status", text="Sync ON"}
+
+    elseif t == WebSocketMessageType.TEXT then
+        dlg:modify{id="status", text="Aseprite Text: " .. message}
+
+    elseif t == WebSocketMessageType.CLOSE then
+        ws:sendText("Aseprite Disconnect")
+        dlg:modify{id="status", text="Connection closed"}
+    end
+end
+
+local function send()
+    ws:sendText("Iam seing some data beepboop")
+end
+-- clean up and exit
+local function finish()
+    ws:close()
+    dlg:close()
+end
+
+-- set up a websocket
+ws = WebSocket{ url="ws://127.0.0.1:8814/Laputa", onreceive=receive, deflate=false }
+--ws://127.0.0.1:8812/Laputa
+-- create an UI
+dlg:label{ id="status", text="Connecting..." }
+dlg:button{ text="Cancel Connection", onclick=finish}
+dlg:combobox {
+    id = "varName3",
+    label = "Attachment: ",
+    option = "Eyes",
+    options = {
+        "Eyes",
+        "Body",
+        "Gloves",
+        "GlovesTop",
+        "Shadow" }
+}
+dlg:button{ text="SendData", onclick=send}
+
+-- let's go!
+ws:connect()
+dlg:show{ wait=false }
+
+------------------------
+
+
+disablePattern = "^-"
+eventPattern = "*-"
+
+local dlg = Dialog { title = "Animation List" }
+function getAnimationTags()
+    local s = app.activeSprite
+    local t = {}
+    for i,tag in ipairs(s.tags) do
+        if not (tag.name:find(disablePattern) ~= nil) and (tag.name:find(eventPattern) ~= nil)  then
+            table.insert(t, tag.name) --.. tostring(tag):gsub("Tag:", "")
+        end
+    end
+    return t
+end
+
+
+dlg:combobox {
+    id = "varName3",
+    label = "Animation: ",
+    option = "Eyes",
+    options = getAnimationTags()
+}
+dlg:show { wait = false }
+
+local function updateTagColors()
+    local s = app.activeSprite
+    for i,tag in ipairs(s.tags) do
+        if (tag.name:find(disablePattern) ~= nil) then
+            tag.color = Color{ r=255, g=255, b=255, a=125 }
+        end
+        if (tag.name:find(disablePattern) ~= nil) then
+            tag.color = Color{ r=255, g=255, b=255, a=125 }
+        end
+    end
+end
+
+
+app.events:on('sitechange',
+        function()
+            updateTagColors()
+        end)
+
+updateTagColors()
